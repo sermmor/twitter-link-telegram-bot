5a27eed048110773242e402e1cc9b327e7142bd0
Sergio Martín <sermmor@outlook.com>
2020-05-08 17:04:26 +0200

    First commit.

---
+++ build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// "npm start" for launch. "npx tsc" for compile Typescript. "node build/index.js" for launch node program.
+// const hello: string = 'hello world!';
+// console.log(hello);
